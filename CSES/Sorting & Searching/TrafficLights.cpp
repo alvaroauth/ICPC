d638689ec255e29442f2e13e10b57cf3119f809c
alvaroauth <alvarofranciscoaufmuth@gmail.com>
2026-07-07 23:53:50 -0300

    .

---
+++ CSES/Sorting & Searching/TrafficLights.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define SZ(x) int((x).size())
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define esta(x,c) ((c).find(x) != (c).end())
+
+int main(){
+    ll x, n; cin >> x >> n;
+
+    multiset<ll> ms;
+    ll aux;
+
+    cin >> aux;
+    ms.insert(aux);
+
+    forn(i, n-1){
+        cin >> aux;
+        
+    }
+    cout << ms.size() << "\n";
+}
